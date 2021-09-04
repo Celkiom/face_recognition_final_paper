81065abc69aa3ec91f812f35a13f2e80944b2896
Celkiom <kiombwecelestin1@gmail.com>
2021-09-04 13:08:38 +0200

    after adding possibility of updating student information by ID

--- myRequests.py
+++ myRequests.py
@@ -26,13 +26,13 @@ def studentInformation():
 def paginationInfo(lim, offs):
     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connect to DB
     mycursor = conn.cursor()
-    mycursor.execute("SELECT matricule, noms, genre, photo, faculte.designation_fac, departement.designation_dep, " \
-                     "promotion.designation_prom, appartenir.annee_academ, finance.solde, workprogram.seance FROM "
-                     "student JOIN appartenir ON student.matricule = appartenir.matricule_fk  JOIN promotion ON "
-                     "appartenir.promotion_fk = promotion.id_prom JOIN departement ON  promotion.departement_fk = "
-                     "departement.id_dep JOIN faculte ON departement.faculte_fk = faculte.id_fac JOIN finance ON "
-                     "student.matricule = finance.matricule_fk JOIN workprogram ON student.matricule = "
-                     "workprogram.matricule_fk LIMIT %s OFFSET %s", (lim, offs))
+    mycursor.execute("SELECT matricule, noms, genre, photo, faculte.designation_fac, departement.designation_dep, "
+                     "promotion.designation_prom, appartenir.annee_academ, finance.solde, workprogram.seance, "
+                     "student.photo FROM student JOIN appartenir ON student.matricule = appartenir.matricule_fk  JOIN "
+                     "promotion ON appartenir.promotion_fk = promotion.id_prom JOIN departement ON "
+                     "promotion.departement_fk = departement.id_dep JOIN faculte ON departement.faculte_fk = "
+                     "faculte.id_fac JOIN finance ON student.matricule = finance.matricule_fk JOIN workprogram ON "
+                     "student.matricule = workprogram.matricule_fk LIMIT %s OFFSET %s", (lim, offs))
     studentsList = mycursor.fetchall()
     mycursor.close()
     return studentsList
@@ -107,124 +107,52 @@ def RegisterStudent(id, Name, Gender, Faculte, Departement, Promotion, Annee, fi
         print("Information not filled", "Please fill informations first...")
         conn.rollback()
 
-# def RegisterStudent(id, Name, Gender, file):
-#     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
-#     mycursor = conn.cursor()
-#     if id != "" and Name != "" and Gender != "" and file != "":
-#         cmd = "INSERT INTO student(matricule, noms, genre, photo) VALUES(%s, %s, %s, %s)"
-#         values = (id, Name, Gender, file)
-#         mycursor.execute(cmd, values)  # Execute the Commande 1
-#         conn.commit()
-#         print("Information registered successfully in database..!")
-#     else:
-#         print("Information not filled", "Please fill informations first...")
-#         conn.rollback()
-
-
-# def RegisterFac(Faculte):
-#     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
-#     mycursor = conn.cursor()
-#     if Faculte != "":
-#         cmd = """INSERT INTO faculte(designation_fac) VALUES(%s)"""
-#         values = (Faculte,)
-#         mycursor.execute(cmd, values)  # Execute the Commande 1
-#         conn.commit()
-#         print("Information registered successfully in database..!")
-#     else:
-#         print("Information not filled", "Please fill informations first...")
-#         conn.rollback()
-#
-#
-# def RegisterFincance(id):
-#     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
-#     mycursor = conn.cursor()
-#     if id != "":
-#         cmd = """INSERT INTO finance(matricule_fk) VALUES(%s)"""
-#         values = (id,)
-#         mycursor.execute(cmd, values)  # Execute the Commande 1
-#         conn.commit()
-#         print("Information registered successfully in database..!")
-#     else:
-#         print("Information not filled", "Please fill informations first...")
-#         conn.rollback()
-#
-#
-# def RegisterDepartem(Departement):
-#     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
-#     mycursor = conn.cursor()
-#     if Departement != "":
-#         mycursor.execute("SELECT id_fac FROM faculte ORDER BY id_fac DESC LIMIT 1")
-#         idFac = mycursor.fetchone()
-#         cmd = """INSERT INTO departement(designation_dep, faculte_fk) VALUES(%s, %s)"""
-#         values = (Departement, idFac[0])
-#         mycursor.execute(cmd, values)  # Execute the Commande 1
-#         conn.commit()
-#         print("Information registered successfully in database..!")
-#     else:
-#         print("Information not filled", "Please fill informations first...")
-#         conn.rollback()
-#
-#
-# def RegisterProm(Promotion):
-#     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
-#     mycursor = conn.cursor()
-#     if Promotion != "":
-#         mycursor.execute("SELECT id_dep FROM departement ORDER BY id_dep DESC LIMIT 1")
-#         idDep = mycursor.fetchone()
-#         cmd = """INSERT INTO promotion(designation_prom, departement_fk) VALUES(%s, %s)"""
-#         values = (Promotion, idDep[0])
-#         mycursor.execute(cmd, values)  # Execute the Commande 1
-#         conn.commit()
-#         print("Information registered successfully in database..!")
-#     else:
-#         print("Information not filled", "Please fill informations first...")
-#         conn.rollback()
-#
-#
-# def RegisterAnnee(id, Annee):
-#     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
-#     mycursor = conn.cursor()
-#     if id != "" and Annee != "":
-#         mycursor.execute("SELECT id_prom FROM promotion ORDER BY id_prom DESC LIMIT 1")
-#         idProm = mycursor.fetchone()
-#         cmd = """INSERT INTO appartenir(annee_academ, promotion_fk, matricule_fk) VALUES(%s, %s, %s)"""
-#         values = (Annee, idProm[0], id)
-#         mycursor.execute(cmd, values)  # Execute the Commande 1
-#         conn.commit()
-#         print("Information registered successfully in database..!")
-#     else:
-#         print("Information not filled", "Please fill informations first...")
-#         conn.rollback()
-#
-#
-# def RegisterWps(id):
-#     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
-#     mycursor = conn.cursor()
-#     if id != "":
-#         cmd = """INSERT INTO workprogram(matricule_fk) VALUES(%s)"""
-#         values = (id,)
-#         mycursor.execute(cmd, values)  # Execute the Commande 1
-#         conn.commit()
-#         print("Information registered successfully in database..!")
-#     else:
-#         print("Information not filled", "Please fill informations first...")
-#         conn.rollback()
-
 
 def deleteStudent(id):
     conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
     mycursor = conn.cursor()
-    cmdDel1 = "DELETE FROM student WHERE matricule = %s"
-    mycursor.execute(cmdDel1, [id])
-    cmdDel2 = "DELETE FROM workprogram WHERE id = %s"
-    mycursor.execute(cmdDel2, [id])
-    cmdDel3 = "DELETE FROM finance WHERE id = %s"
-    mycursor.execute(cmdDel3, [id])
-    cmdDel4 = "DELETE FROM appartenir WHERE promotion_fk = %s"
-    mycursor.execute(cmdDel4, [id])
-    student = conn.commit()
+    mycursor.execute("SELECT promotion_fk FROM appartenir WHERE matricule_fk =" + str(id))
+    idprom = mycursor.fetchone()
+    mycursor.execute("SELECT departement_fk FROM promotion WHERE id_prom =" + str(idprom[0]))
+    iddep = mycursor.fetchone()
+    mycursor.execute("SELECT faculte_fk FROM departement WHERE id_dep =" + str(iddep[0]))
+    idfac = mycursor.fetchone()
+    mycursor.execute("DELETE FROM workprogram WHERE matricule_fk = %s" % id)
+    mycursor.execute("DELETE FROM finance WHERE matricule_fk = %s" % id)
+    mycursor.execute("DELETE FROM appartenir WHERE matricule_fk = %s" % id)
+    mycursor.execute("DELETE FROM promotion WHERE id_prom = %s" % idprom[0])
+    mycursor.execute("DELETE FROM departement WHERE id_dep = %s" % iddep[0])
+    mycursor.execute("DELETE FROM faculte WHERE id_fac = %s" % idfac[0])
+    mycursor.execute("DELETE FROM student WHERE matricule = %s" % id)
+    students = conn.commit()
     print("record deleted with success...")
-    return student
+    return students
+
+
+def editStudent(id, noms, faculte, departement, promotion, annee, file):
+    conn = mysql.connector.connect(host="localhost", database="memoire", user="root", password="")  # Connection to DB
+    mycursor = conn.cursor()
+    mycursor.execute("SELECT promotion_fk FROM appartenir WHERE matricule_fk =" + str(id))
+    idprom = mycursor.fetchone()
+    mycursor.execute("SELECT departement_fk FROM promotion WHERE id_prom =" + str(idprom[0]))
+    iddep = mycursor.fetchone()
+    mycursor.execute("SELECT faculte_fk FROM departement WHERE id_dep =" + str(iddep[0]))
+    idfac = mycursor.fetchone()
+
+    cmd = "UPDATE student SET noms = %s,photo = %s WHERE matricule =%s"
+    value = (noms, file, id)
+    mycursor.execute(cmd, value)
+    mycursor.execute("UPDATE appartenir SET annee_academ = {0} WHERE matricule_fk ={1}".format(annee, id))
+    cmd1 = "UPDATE faculte SET designation_fac =%s WHERE id_fac =%s"
+    value1 = (faculte, idfac[0])
+    mycursor.execute(cmd1, value1)
+    cmd2 = "UPDATE departement SET designation_dep =%s WHERE id_dep =%s"
+    value2 = (departement, iddep[0])
+    mycursor.execute(cmd2, value2)
+    cmd3 = "UPDATE promotion SET designation_prom =%s WHERE id_prom =%s"
+    value3 = (promotion, idprom[0])
+    mycursor.execute(cmd3, value3)
+    conn.commit()
 
 
 def takePic(id, name):
--- recogServer.py
+++ recogServer.py
@@ -1,8 +1,9 @@
 from base64 import b64encode
 import cv2
 import mysql
-from flask import Flask, render_template, request, Response, url_for, flash
+from flask import Flask, render_template, request, Response, url_for, flash, redirect
 from flask_paginate import get_page_parameter, Pagination
+
 import myRequests
 from camera import VideoCamera
 import test
@@ -10,13 +11,14 @@ import test
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 static_url_path = '/static'
+app.secret_key = 'key secret'
 
 db = test.db
 
 path = './images'
 img_path = []
 # for pagination
-limit = 5  # perPage
+limit = 6  # perPage
 
 rows = myRequests.studentInformation()
 total = len(rows)
@@ -46,35 +48,18 @@ def registeredSt():
 @app.route("/registerInfo", methods=['POST', 'GET'])
 def registerStudent():
     if request.method == 'POST':
-        if not request.form['id'] or not request.form['noms'] or not request.form['genre'] or not request.form['faculte'] or not request.form['departement'] or not request.form['promotion'] or not request.files['file']:
-            flash('Ples enter all the fields', 'error')
-        else:
-            Matricule = request.form["id"]
-            Noms = request.form["noms"]
-            Genre = request.form["genre"]
-            Faculte = request.form["faculte"]
-            Departement = request.form["departement"]
-            Promotion = request.form["promotion"]
-            Annee = request.form["annee"]
-            files = request.files["file"]
-            file = str(files)
-            # etudiantOb = test.Etudiant(matricule=Matricule, nom=Noms, genre=Genre, photo=file)
-            # studentObj = test.Student(Matricule, Noms, Genre, file)
+        Matricule = request.form["id"]
+        Noms = request.form["noms"]
+        Genre = request.form["genre"]
+        Faculte = request.form["faculte"]
+        Departement = request.form["departement"]
+        Promotion = request.form["promotion"]
+        Annee = request.form["annee"]
+        files = request.files["file"]
+        file = str(files)
         if request.form["submit"] == "SaveInfo":
             myRequests.RegisterStudent(Matricule, Noms, Genre, Faculte, Departement, Promotion, Annee, file)
-            # myRequests.RegisterFac(Faculte)
-            # myRequests.RegisterFincance(Matricule)
-            # myRequests.RegisterDepartem(Departement)
-            # myRequests.RegisterProm(Promotion)
-            # myRequests.RegisterAnnee(Matricule, Annee)
-            # myRequests.RegisterWps(Matricule)
-
-            # promotionOb = test.Promotion.query.all().first()
-
-            # db.session.add(studentObj)
-            # db.session.commit()
-
-            flash('Reccord was successfully added')
+            flash(' '+Noms+' was successfully added')
 
     # Pagination management
     page = request.args.get(get_page_parameter(), type=int, default=1)
@@ -94,9 +79,34 @@ def attendanceFinger():
     return render_template('attendance.html')
 
 
-@app.route("/studentInfo/<int:id>", methods=['POST', 'GET'])
+@app.route("/registerInfo/<int:id>", methods=['POST', 'GET'])
 def deleteStudent(id):
-    myRequests.deleteStudent(id)
+    if request.method == 'POST':
+        if request.form["submit"] == "delete":
+            myRequests.deleteStudent(id)
+            flash(' with ID '+id+' was deleted successfully in the database..:)')
+    # Pagination management
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    offset = page * limit - limit
+    studentsList = myRequests.paginationInfo(limit, offset)
+    pagination = Pagination(page=page, per_page=limit, total=total, css_framework='bootstrap4')
+    return render_template('studentInfo.html', student=studentsList, pagination=pagination, page=page)
+
+
+@app.route("/editInfo", methods=['POST', 'GET'])
+def editStudent():
+    if request.method == 'POST':
+        Matricule = request.form["idEdit"]
+        Noms = request.form["nomsEdit"]
+        Faculte = request.form["faculteEdit"]
+        Departement = request.form["departementEdit"]
+        Promotion = request.form["promotionEdit"]
+        Annee = request.form["anneeEdit"]
+        files = request.files["fileEdit"]
+        file = str(files)
+        if request.form['submit'] == "SaveEdit":
+            myRequests.editStudent(Matricule, Noms, Faculte, Departement, Promotion, Annee, file)
+            flash(' '+Noms+'\'s informations was updated successfully..:)')
     # Pagination management
     page = request.args.get(get_page_parameter(), type=int, default=1)
     offset = page * limit - limit
